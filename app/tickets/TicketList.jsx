0311f53cc36b410b5e4181b9e5cfba2eabf23b4c
+ <+>
2024-02-28 10:43:13 +0300

    Loading UI &Suspense

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 async function getTickets(){
-    const res = await fetch("http://localhost:4000/tickets",{
+      await new Promise(resolve => setTimeout(resolve, 3000))
+      const res = await fetch("http://localhost:4000/tickets",{
       next: {
         revalidate: 30
       }
